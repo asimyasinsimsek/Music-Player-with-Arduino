771c63915f46e5a08ed2f07746548e3a8acfee2d
Asım Yasin ŞİMŞEK <94120023+asimyasinsimsek@users.noreply.github.com>
2023-03-09 23:31:42 +0300

    Add files via upload

---
+++ musicplayer.ino
@@ -0,0 +1,895 @@
+#define do_ 261
+#define re 294
+#define mi 329
+#define fa 349
+#define sol 392
+#define la 440
+#define si 493
+#define DOO 523
+int kalindo = 261;
+
+
+#define buzzer 7
+
+int buton1 = 8;
+int buton2 = 9;
+int buton3 = 10;
+void setup() {
+   pinMode(buzzer,OUTPUT);
+   pinMode(buton1,INPUT); 
+  
+
+}
+
+void loop() {
+  //cendere
+ if ( digitalRead(buton1)==HIGH )
+   {
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(700);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(700);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(750);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+
+   tone(buzzer, si);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+
+  tone(buzzer, si);
+  delay(550);
+  noTone(buzzer);
+  delay(10);
+
+  tone(buzzer, la);
+  delay(900);
+  noTone(buzzer);
+  delay(10);
+ 
+ 
+  
+ 
+  delay(1500);
+}
+//izmir marsi
+if ( digitalRead(buton2)==HIGH )
+   {
+  tone(buzzer, la);
+  delay(600);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(700);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(600);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(700);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(700);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(700);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(700);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, DOO);
+  delay(600);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(600);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(700);
+  noTone(buzzer);
+  delay(10);
+}
+//askuryala
+if(digitalRead(buton3)==HIGH)
+{
+  tone(buzzer, si);
+  delay(700);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(700);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(800);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, do_);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+
+  tone(buzzer, si);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, la);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, re);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, mi);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, fa);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+ 
+  tone(buzzer, sol);
+  delay(400);
+  noTone(buzzer);
+  delay(10);
+
+  tone(buzzer, mi);
+  delay(700);
+  noTone(buzzer);
+  delay(10);
+
+  
+ 
+ 
+  delay(1500);
+}
+
+}
